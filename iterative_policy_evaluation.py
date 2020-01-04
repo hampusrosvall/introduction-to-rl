6bdc17d3f189e960c4a86c18494cd1d44e496864
Hampus Rosvall <hampusrosvall@hr.lan>
2020-01-04 20:07:54 +0100

    first commit

---
+++ iterative_policy_evaluation.py
@@ -0,0 +1,27 @@
+import numpy as np
+from grid_world import standard_grid
+
+SMALL_ENOUGH = 1e-3 # threshold for convergence
+
+def print_values(V, g):
+  for i in range(g.rows):
+    print("---------------------------")
+    for j in range(g.cols):
+      v = V.get((i,j), 0)
+      if v >= 0:
+        print(" %.2f|" % v, end="")
+      else:
+        print("%.2f|" % v, end="") # -ve sign takes up an extra space
+    print("")
+
+
+def print_policy(P, g):
+  for i in range(g.rows):
+    print("---------------------------")
+    for j in range(g.cols):
+      a = P.get((i,j), ' ')
+      print("  %s  |" % a, end="")
+    print("")
+
+if __name__ == '__main__':
+    grid = standard_grid()
